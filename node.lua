21edb9ecc3ce2a493aa1f8dfc4e6624b60dd8008
Linus <linus.penzlien@googlemail.com>
2019-08-30 11:46:05 +0200

    ..

--- node.lua
+++ node.lua
@@ -15,8 +15,9 @@ end,
 
 
 function node.render()
-    video:draw(960, 540, WIDTH, HEIGHT)
-    font:write(30,10, "MORE Motion diggha", 100, .5, .5, .5,1)
+    gl.clear(0,0,0,1)
+    font:write(30, 10, "Motion Detected", 100, .5,.5,.5,1)
     countStr = tostring(count)
     font:write(250, 300, countStr, 64, 1,1,1,1)
+    video:draw(960, 540, WIDTH, HEIGHT)
 end
