54eb16fa45527e79c9f63efa7c43764849a3407c
Linus <earsneyes@users.noreply.github.com>
2019-08-30 12:54:04 +0200

    Update node.lua

--- node.lua
+++ node.lua
@@ -1,4 +1,5 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+
 local font = resource.load_font "font.ttf"
 local count = 0
 
@@ -16,8 +17,5 @@ end,
 
 function node.render()
     gl.clear(0,0,0,1)
-    font:write(30, 10, "Motion Detected", 100, .5,.5,.5,1)
-    countStr = tostring(count)
-    font:write(250, 300, countStr, 64, 1,1,1,1)
-    video:draw(960, 540, WIDTH, HEIGHT)
+    video:draw(0, 0, WIDTH, HEIGHT)
 end
