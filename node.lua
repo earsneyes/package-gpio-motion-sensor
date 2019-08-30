1bc4634d836488d9d5b00f4f48994a1fa3ef88fe
Linus <linus.penzlien@googlemail.com>
2019-08-30 11:22:18 +0200

    display counter as well

--- node.lua
+++ node.lua
@@ -1,10 +1,18 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
-local video = resource.load_video{
+local count = 0
 
+local video = resource.load_video{
     file = "helloworld.mp4";
     looped = false;
 }
+
+util.data_mapper{
+    counter = function(counter)
+        count = counter
+end,
+}
+
 
 function node.render()
     video:draw(0, 0, WIDTH, HEIGHT)
